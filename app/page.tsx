da0fca1118ad51e82cb3b9bd6bdcd116bd6681b3
chris251190 <christian_lehr.1@gmx.de>
2023-12-27 13:25:31 +0100

    Refactor page layout and remove unnecessary code

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,8 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-2xl font-bold mb-3">Next Appointment:</h1>
       <div className="flex">
         <input
           type="text"
