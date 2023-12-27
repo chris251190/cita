bac611e122f15a7e381dd8270f1d736e76e7148e
chris251190 <christian_lehr.1@gmx.de>
2023-12-27 13:09:25 +0100

    Update input styling in page component

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <input type="date" className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black" />
-      </main>
+      <input type="date" className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black" />
+    </main>
   )
 }
