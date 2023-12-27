b49970072342683599d0dfb2d5aceeabdf221ec9
chris251190 <christian_lehr.1@gmx.de>
2023-12-27 13:15:57 +0100

    Add input fields and button for user input and display

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,45 @@
+"use client"
+import React, { useState } from 'react';
+
 export default function Home() {
+  const [name, setName] = useState('');
+  const [selectedDate, setSelectedDate] = useState('');
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedDate(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    console.log(`Name: ${name}, Selected Date: ${selectedDate}`);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <input type="date" className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black" />
+      <div className="flex">
+        <input
+          type="text"
+          className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+          placeholder="Enter your name"
+          value={name}
+          onChange={handleNameChange}
+        />
+        <input
+          type="date"
+          className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+          value={selectedDate}
+          onChange={handleDateChange}
+        />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handleButtonClick}
+        >
+          Display
+        </button>
+      </div>
     </main>
-  )
+  );
 }
