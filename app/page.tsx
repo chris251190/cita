80f87a3fca26904384df9bb8345446a8b754df58
chris251190 <christian_lehr.1@gmx.de>
2023-12-27 13:18:40 +0100

    Replace "Display" with the checkmark icon

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           onClick={handleButtonClick}
         >
-          <FaCheck /> {/* Replace "Display" with the checkmark icon */}
+          <FaCheck />
         </button>
       </div>
     </main>
