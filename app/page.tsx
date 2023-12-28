83c88ae1c1cf220c2cb1d7a5bd36989dc8405f68
chris251190 <christian_lehr.1@gmx.de>
2023-12-28 13:18:30 +0100

    Refactor appointment rendering and add QR code

--- app/page.tsx
+++ app/page.tsx
@@ -87,27 +87,25 @@ export default function Home() {
           </button>
         </form>
       </div>
-      
+
       <div>
         {appointments.map((appointment, index) => (
           <div key={index} className="flex items-center">
             <p className='mr-5 mt-3 mb-3'>{appointment.name}, {new Date(appointment.selectedDate).toLocaleDateString()} - {appointment.selectedTime}</p>
+
             <button
               className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
               onClick={() => handleRemoveAppointment(index)}
             >
               <FaTimes />
             </button>
+            <QRCode
+              className="mb-5"
+              value={formatGoogleCalendarURL(lastAppointment)}
+            />
           </div>
         ))}
       </div>
-
-      {showQRCode && lastAppointment && (
-        <QRCode
-          className="mb-5"
-          value={formatGoogleCalendarURL(lastAppointment)}
-        />
-      )}
     </main>
   );
 }
