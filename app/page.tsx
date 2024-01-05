0c1966ebac6f20dbede16c72f651ae53d893eddb
Christian Lehr <c.lehr@myeosgroup.net>
2024-01-05 23:06:32 +0100

    Refactor InputField component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -25,15 +25,16 @@ type InputFieldProps = {
 const InputField: React.FC<InputFieldProps> = ({ type, name, placeholder, value, onChange, additionalClasses, title }) => (
   <>
     {title && <label className="font-bold mb-1">{title}</label>}
-    <input
-      type={type}
-      name={name}
-      className={`bg-transparent border-b border-black focus:outline-none focus:ring-0 focus:border-orange-600 hover:border-orange-600 hover:text-lg text-black placeholder-orange-600 mr-2 mb-5 ${additionalClasses}`}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-    />
-  </>
+      <input
+        type={type}
+        name={name}
+        className={`bg-transparent border-b border-black focus:outline-none focus:ring-0 focus:border-orange-600 hover:border-orange-600 text-black placeholder-orange-600 mr-2 mb-5 ${additionalClasses} text-center hover:scale-110`}
+        placeholder={placeholder}
+        style={{ width: '120px' }}
+        value={value} 
+        onChange={onChange}
+      />
+    </>
 );
 
 export default function Cita() {
