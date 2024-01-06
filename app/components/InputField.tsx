837f9d50e52b392a9915cbc90ddcb327e113946e
Christian Lehr <c.lehr@myeosgroup.net>
2024-01-06 09:41:12 +0100

    Add InputField component

---
+++ app/components/InputField.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+type InputFieldProps = {
+  title?: string;
+  additionalClasses?: string;
+  type: string;
+  name: string;
+  placeholder?: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const InputField: React.FC<InputFieldProps> = ({ type, name, placeholder, value, onChange, additionalClasses, title }) => (
+  <>
+    {title && <label className="font-bold mb-1">{title}</label>}
+    <input
+      type={type}
+      name={name}
+      className={`bg-transparent border-b border-black focus:outline-none focus:ring-0 focus:border-orange-600 hover:border-orange-600 text-black placeholder-orange-600 mb-5 ${additionalClasses} text-center hover:scale-110`}
+      placeholder={placeholder}
+      style={{ width: '150px' }}
+      value={value}
+      onChange={onChange}
+    />
+  </>
+);
+
+export default InputField;
