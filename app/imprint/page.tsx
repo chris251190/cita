4bd22e5d5b54c463d3ee227f4bbe598422a44b51
Christian Lehr <c.lehr@myeosgroup.net>
2024-01-07 09:56:29 +0100

    Refactor imprint page layout

--- app/imprint/page.tsx
+++ app/imprint/page.tsx
@@ -3,20 +3,21 @@ import Footer from '../components/Footer';
 import Logo from '../components/Logo';
 
 const Imprint = () => (
-    <div className='flex flex-col items-center'>
+    <div className='flex flex-col items-center text-center'>
         <Logo />
-        <h2 className="font-bold">Responsible For Content</h2>
 
-        <p>Christian Lehr</p>
-        <p>Bramfelder Dorfplatz 8</p>
-        <p>22179 Hamburg</p>
-        <p>Phone: +4915202685626</p>
-        <p>Email: christian_lehr.1@gmx.de</p>
+        <div>
+        <h2 className="font-bold mb-3">Responsible For Content</h2>
+
+            <p>Christian Lehr</p>
+            <p>Bramfelder Dorfplatz 8</p>
+            <p>22179 Hamburg</p>
+            <p>Phone: +4915202685626</p>
+            <p>Email: christian_lehr.1@gmx.de</p>
+        </div>
 
         <Footer />
     </div>
 );
-
-
 
 export default Imprint;
