8f05c36270add948645928975a9895aa86f9a7fe
Christian Lehr <c.lehr@myeosgroup.net>
2024-01-07 21:41:07 +0100

    Add new launcher icon for Android

--- app/manifest.ts
+++ app/manifest.ts
@@ -32,6 +32,12 @@ export default function manifest(): MetadataRoute.Manifest {
                 type: 'image/png',
                 purpose: 'maskable'
             },
+            {
+                src: '/android-launchericon-512-512.png', 
+                sizes: '512x512', 
+                type: 'image/png', 
+                purpose: 'any'
+            },
         ],
         screenshots: [
             {
