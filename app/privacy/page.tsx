82ccec63f81c34e663f99c0a7c4b7136ab0bf6c5
Christian Lehr <c.lehr@myeosgroup.net>
2024-01-07 09:15:50 +0100

    Update privacy page heading

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -2,21 +2,8 @@ import React from 'react';
 
 const Privacy = () => (
     <div>
-    <h1>Privacy Policy</h1>
-    <p>Your Company ("us", "we", or "our") operates the http://www.yourwebsite.com website (the "Service").</p>
-
-    <h2>Information Collection And Use</h2>
-    <p>We collect several different types of information for various purposes to provide and improve our Service to you.</p>
-
-    <h2>Types of Data Collected</h2>
-    <h3>Personal Data</h3>
-    <p>While using our Service, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you ("Personal Data"). Personally identifiable information may include, but is not limited to: Email address, Name, Address, State, Province, ZIP/Postal code, City, Cookies and Usage Data.</p>
-
-    // Continue with other sections like Log Data, Cookies, Service Providers, etc.
-
-    <h2>Changes To This Privacy Policy</h2>
-    <p>We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
-  </div>
+        <h2>Privacy Policy</h2>
+    </div>
 );
 
 export default Privacy;
