7d9739bfc93146761606058dc8a96fd70b9cccfe
Christian Lehr <c.lehr@myeosgroup.net>
2024-01-07 11:55:39 +0100

    Refactor Footer component to add hover effect to links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,11 +1,10 @@
 import Link from "next/link";
 
 const Footer = () => {
-        return (
-            <div className="mt-8">
-                 <Link href="/">Home</Link> | <Link href="/privacy">Privacy</Link> | <Link href="/imprint">Imprint</Link>
-            </div>
-        );
+    return (
+        <div className="mt-8">
+<Link className="hover:text-orange-700" href="/">Home</Link> | <Link className="hover:text-orange-700" href="/privacy">Privacy</Link> | <Link className="hover:text-orange-700" href="/imprint">Imprint</Link>        </div>
+    );
 }
 
 export default Footer;
