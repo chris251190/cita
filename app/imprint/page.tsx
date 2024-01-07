f67c79ecbbfa55f7d3a9f03e7a8373406c687320
Christian Lehr <c.lehr@myeosgroup.net>
2024-01-07 09:14:46 +0100

    Add contact information to Imprint page

--- app/imprint/page.tsx
+++ app/imprint/page.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Imprint = () => (
     <div>
-        Imprint
-    </div>
+    <h2>Responsible For Content</h2>
+
+    <p>Christian Lehr</p>
+    <p>Bramfelder Dorfplatz 8</p>
+    <p>22179 Hamburg</p>
+    <p>Phone: +4915202685626</p>
+    <p>Email: christian_lehr.1@gmx.de</p>
+  </div>
 );
 
 export default Imprint;
