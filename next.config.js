d2bf3571c4916f1c3d7ffee5f6200fe2d96b6c69
Christian Lehr <c.lehr@myeosgroup.net>
2024-01-07 22:37:04 +0100

    Update next.config.js with PWA configuration

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
+const runtimeCaching = require("next-pwa/cache");
+
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
   disable: process.env.NODE_ENV === "development",
 })
 
 module.exports = withPWA({
   // next.js config
-  reactStrictMode: false,
+  reactStrictMode: true,
   swcMinify: false,
 })
