f337ad353348533555d6eb56f9cb194ec9b2ca66
Christian Lehr <c.lehr@myeosgroup.net>
2024-01-07 20:17:29 +0100

    Update manifest.ts with additional icons

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,20 +1,35 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from "next";
+
 export default function manifest(): MetadataRoute.Manifest {
-  return {
-    name: 'Cita',
-    short_name: 'Cita',
-    description: 'The easiest way to create and share your appointments.',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#fff',
-    theme_color: '#fff',
-    icons: [
-      {
-        src: '/favicon.ico',
-        sizes: 'any',
-        type: 'image/x-icon',
-      },
-    ],
-  }
+    return {
+        name: 'Cita',
+        short_name: 'Cita',
+        description: 'The easiest way to create and share your appointments.',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#fff',
+        theme_color: '#fff',
+        icons: [
+            {
+                src: '/favicon.ico',
+                sizes: 'any',
+                type: 'image/x-icon',
+            },
+            {
+                src: '/screenshot_lg.png',
+                sizes: '1920x1080',
+                type: 'image/png',
+            },
+            {
+                src: '/screenshot_sm.png',
+                sizes: '1280x720',
+                type: 'image/png',
+            },
+            {
+                src: '/icon.png',
+                sizes: '512x512',
+                type: 'image/png',
+            },
+        ],
+    }
 }
