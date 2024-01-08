7be9af608d62ee0ae9ce9035625c57ec9e28fd8b
Christian Lehr <c.lehr@myeosgroup.net>
2024-01-08 19:04:41 +0100

    Add profile picture and update About page content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { FaInstagram, FaGithub } from 'react-icons/fa';
+import Image from 'next/image';
 
 const AboutPage = () => {
     return (
-        <div>
-            <h1>About Page</h1>
-            <p>Welcome to the About page!</p>
+        <div className="flex justify-center items-center h-screen">
+            <div className="text-center max-w-2xl mx-auto">
+                <h1 className="text-2xl font-bold mb-4">Hey there! 👋</h1>
+
+                <Image src="/me.jpeg" width={200} height={200} alt="Profile Picture" className="rounded-full mx-auto mb-4" />
+
+                <p className="mb-4">Welcome to Cita, The easiest way to create and share appointments!</p>
+                <p className="mb-4">I am Christian, a passionate software developer from beautiful Hamburg with expertise in Java Spring and Next.js. I love building innovative and user-friendly applications that solve real-world problems while always learning something new.</p>
+                <p className="mb-4">When I'm not coding I love to stand up paddle, play squash with friends or do some gaming on Switch/PC. Additionally I'm a big anime fan ❤️ and try to raise more awareness about veganism. 🌱🐮🐷🐤🐟</p>
+                <p className="mb-4">I created Cita to help people create and share appointments with their friends and family. I hope you enjoy using it!</p>
+                <p className="mb-4">Feel free to connect with me on social media:</p>
+                <div className="flex justify-center items-center">
+                    <a href="https://www.instagram.com/jinfroftw" className="mr-4">
+                        <FaInstagram size={40} />
+                    </a>
+                    <a href="https://github.com/chris251190">
+                        <FaGithub size={40} />
+                    </a>
+                </div>
+            </div>
         </div>
     );
 };
--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <div className="mt-8">
-<Link className="hover:text-orange-700" href="/">Home</Link> | <Link className="hover:text-orange-700" href="/privacy">Privacy</Link> | <Link className="hover:text-orange-700" href="/imprint">Imprint</Link>        </div>
+<Link className="hover:text-orange-700" href="/">Home</Link> | <Link className="hover:text-orange-700" href="/about">By <b>Christian Lehr</b></Link> | <Link className="hover:text-orange-700" href="/faq">FAQ</Link> | <Link className="hover:text-orange-700" href="/privacy">Privacy</Link> | <Link className="hover:text-orange-700" href="/imprint">Imprint</Link>        </div>
     );
 }
 
