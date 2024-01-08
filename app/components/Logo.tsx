883034211ffd1b5d0fcca01b707dfd4803df39f4
Christian Lehr <c.lehr@myeosgroup.net>
2024-01-08 20:48:36 +0100

    Add link to logo in Logo component

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Logo = () => {
   return (
     <div className="w-full flex justify-center">
-      <Image src="/Cita.png" alt="Logo" width={200} height={200} priority />
+      <Link href="/">
+          <Image src="/Cita.png" alt="Logo" width={200} height={200} priority />
+      </Link>
     </div>
   );
 };
